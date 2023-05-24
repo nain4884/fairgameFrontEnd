fa2eaafc7865ce9710375ed6b518b5aad6c7fa8f
saleelhisan <saleelhisan83@gmail.com>
2023-05-24 19:06:24 +0530

    list transaction history

--- src/components/AccountStatementList.js
+++ src/components/AccountStatementList.js
@@ -2,8 +2,47 @@ import { Box, Typography } from "@mui/material"
 import SearchInput from "./SearchInput"
 import { Excel, LockIcon, Pdf, UnLockIcon } from "../assets"
 import SmallDropDown from "./smallDropDown"
+import { useSelector } from "react-redux"
+import { useEffect, useState } from "react"
+import constants from "./helper/constants"
+import { setRole } from "../newStore"
+import { toast } from "react-toastify"
 
 const AccountStatementList = () => {
+    const { currentUser } = useSelector((state) => state?.currentUser);
+
+    const [pageLimit, setPageLimit] = useState(constants.pageLimit);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [transactionHistory, setTransactionHistory] = useState([])
+    async function getAccountStatement() {
+        const userId = currentUser.id
+        console.log(userId)
+        let payload = {
+            limit: pageLimit,
+            skip: currentPage
+        };
+        let { axios } = setRole();
+        try {
+            const { data } = await axios.post(
+                `/fair-game-wallet/transactionHistory/${userId}`, payload
+            );
+            console.log(data.data[0], 'datadatadatadata')
+            setTransactionHistory(data.data[0])
+    //   toast.success(data?.message);
+
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+
+    useEffect(() => {
+        getAccountStatement()
+    }, []);
+
+
+
     const Footer = () => {
         return (
             <Box sx={{ height: "50px", display: "flex", alignItems: "center", px: { mobile: "5px", laptop: "10px" }, justifyContent: "space-between", background: "#FAFAFA", }}>
@@ -30,18 +69,28 @@ const AccountStatementList = () => {
             <Box sx={{ overflowX: 'scroll' }}>
                 <Box sx={{ overflowX: 'scroll', minWidth: '900px' }}>
                     <ListHeaderT />
-
-                    <Row index={1} containerStyle={{ background: "#FFE094" }} profit={true} fContainerStyle={{ background: "#0B4F26" }} fTextStyle={{ color: "white" }} />
-                    <Row index={2} containerStyle={{ background: "#FFE094" }} profit={false} fContainerStyle={{ background: "#F8C851" }} fTextStyle={{ color: "#0B4F26" }} />
-                    <Row index={3} containerStyle={{ background: "#FFE094" }} profit={true} fContainerStyle={{ background: "#0B4F26" }} fTextStyle={{ color: "white" }} />
-                    <Row index={4} containerStyle={{ background: "#FFE094" }} profit={false} fContainerStyle={{ background: "#F8C851" }} fTextStyle={{ color: "#0B4F26" }} />
-                    <Row index={5} containerStyle={{ background: "#FFE094" }} profit={true} fContainerStyle={{ background: "#0B4F26" }} fTextStyle={{ color: "white" }} />
-                    <Row index={6} containerStyle={{ background: "#FFE094" }} profit={false} fContainerStyle={{ background: "#F8C851" }} fTextStyle={{ color: "#0B4F26" }} />
-                    <Row index={7} containerStyle={{ background: "#FFE094" }} profit={true} fContainerStyle={{ background: "#0B4F26" }} fTextStyle={{ color: "white" }} />
-                    <Row index={8} containerStyle={{ background: "#FFE094" }} profit={false} fContainerStyle={{ background: "#F8C851" }} fTextStyle={{ color: "#0B4F26" }} />
+                    {transactionHistory.map((item) => (
+                         <Row
+                         index={1}
+                         containerStyle={{ background: "#FFE094" }}
+                         profit={true}
+                         fContainerStyle={{ background: "#0B4F26" }}
+                         fTextStyle={{ color: "white" }}
+                         date={item.createAt}
+                         closing={item.current_amount}
+                         trans_type={item.trans_type}
+                         amount={item.amount}
+                         touserName={item.action_by.userName}
+                         fromuserName={item.user.userName}
+                         {...(item.trans_type === "withdraw" ? { debit: item.amount } : { credit: item.amount })}
+                         {...(item.trans_type === "add" ? { fromuserName: item.action_by.userName , touserName: item.user.userName } : {fromuserName: item.user.userName ,touserName: item.action_by.userName})}           
+                         />
+                        
+                        // <Row index={1} containerStyle={{ background: "#FFE094" }} profit={true} fContainerStyle={{ background: "#0B4F26" }} fTextStyle={{ color: "white" }} date={item.createAt} closing={item.current_amount} trans_type={item.trans_type} amount={item.amount} touserName={item.action_by.userName} fromuserName={item.user.userName} />
+                    ))}
                 </Box>
             </Box>
-            <Footer />
+            {/* <Footer /> */}
         </Box>
     )
 }
@@ -89,29 +138,38 @@ const ListHeaderT = () => {
 
 
 
-const Row = ({ containerStyle, fContainerStyle, fTextStyle, profit, index }) => {
+const Row = ({ containerStyle, fContainerStyle, fTextStyle, profit, index, date, closing, description, touserName,fromuserName, trans_type, amount,debit,credit }) => {
+
+    const dateString = date;
+    const dateObj = new Date(dateString);
+    const formattedDate = dateObj.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit"
+    });
+
     return (
         <Box sx={[{ display: "flex", height: "45px", background: "#0B4F26", alignItems: "center", overflow: "hidden", borderBottom: "2px solid white" }, containerStyle]}>
             <Box sx={[{ width: "11%", display: "flex", paddingLeft: "10px", alignItems: "center", height: "45px", borderRight: "2px solid white" }, fContainerStyle]}>
-                <Typography sx={[{ fontSize: "12px", fontWeight: "600" }, fTextStyle]}>03-11-2022</Typography>
+                <Typography sx={[{ fontSize: "12px", fontWeight: "600" }, fTextStyle]}>{formattedDate}</Typography>
             </Box>
             <Box sx={{ width: "14%", display: "flex", paddingLeft: "10px", background: "#27AC1E", alignItems: "center", height: "45px", borderRight: "2px solid white" }}>
-                <Typography sx={{ fontSize: "12px", fontWeight: "600", color: "white" }}>4,02,000,000,0</Typography>
+                <Typography sx={{ fontSize: "12px", fontWeight: "600", color: "white" }}>{credit}</Typography>
             </Box>
             <Box sx={{ width: "14%", display: "flex", paddingLeft: { laptop: "10px", mobile: "5px" }, background: "#E32A2A", alignItems: "center", height: "45px", borderRight: "2px solid white" }}>
-                <Typography sx={{ fontSize: "12px", fontWeight: "600", color: "white" }}>4,02,000,000,0</Typography>
+                <Typography sx={{ fontSize: "12px", fontWeight: "600", color: "white" }}>{debit}</Typography>
             </Box>
             <Box sx={{ width: "14%", display: "flex", paddingLeft: "10px", alignItems: "center", height: "45px", borderRight: "2px solid white", background: index % 2 != 0 ? "#FFE094" : '#ECECEC' }}>
-                <Typography sx={{ fontSize: "12px", fontWeight: "600" }}>1,00,000,000,0</Typography>
+                <Typography sx={{ fontSize: "12px", fontWeight: "600" }}>{closing}</Typography>
             </Box>
             <Box sx={{ width: "36%", display: "flex", paddingLeft: "10px", alignItems: "center", height: "45px", borderRight: "2px solid white", background: index % 2 != 0 ? "#FFE094" : '#ECECEC' }}>
-                <Typography sx={{ fontSize: "12px", fontWeight: "500" }}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam</Typography>
+                <Typography sx={{ fontSize: "12px", fontWeight: "500" }}>{trans_type}</Typography>
             </Box>
             <Box sx={{ width: "11%", display: "flex", paddingLeft: "10px", alignItems: "center", height: "45px", borderRight: "2px solid white", justifyContent: 'center', background: index % 2 != 0 ? "#FFE094" : '#ECECEC' }}>
-                <Typography sx={{ fontSize: "12px", fontWeight: "700" }}>Admin</Typography>
+                <Typography sx={{ fontSize: "12px", fontWeight: "700" }}>{fromuserName}</Typography>
             </Box>
             <Box sx={{ width: "11%", display: "flex", justifyContent: "center", alignItems: "center", height: "45px", borderRight: "2px solid white", background: index % 2 != 0 ? "#FFE094" : '#ECECEC' }}>
-                <Typography sx={{ fontSize: "12px", fontWeight: "700" }}>Nika 20</Typography>
+                <Typography sx={{ fontSize: "12px", fontWeight: "700" }}>{touserName}</Typography>
 
             </Box>
 
