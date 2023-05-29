d2cba548cc0d46747f3d29997dfcb32b36589178
Md Azim Babu <101165102+ajay-duple@users.noreply.github.com>
2023-05-29 20:04:16 +0530

    Update PlaceBet.js

--- src/components/PlaceBet.js
+++ src/components/PlaceBet.js
@@ -432,7 +432,7 @@ const PlaceBet = ({
           marginLeft: season ? "40px" : 0,
           overflow: "hidden",
 
-          width: { mobile: "98vw", tablet: "60vw", laptop: "30vw" },
+          width: { mobile: "98vw", tablet: "60vw", laptop: "40%" },
           // boxShadow:
           //   "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
           boxShadow: "rgba(0, 0, 0, 0.56) 0px 22px 70px 4px"
