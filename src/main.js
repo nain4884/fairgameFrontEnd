76e5b9c8113e034d5aec94dcac0849efd47a059c
atul <atul.duple@gmail.com>
2023-06-23 15:02:59 +0530

    fix

--- src/main.js
+++ src/main.js
@@ -1,45 +1,39 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import React, { Suspense } from "react";
 import Matches from "./pages/matches";
+import MasterRoutes from "./pages/master";
+import ExpertRoutes from "./pages/expert";
 
 import { AuthProvider } from "./Authprovider";
 import ForgotPassword from "./pages/ForgotPassword";
 import Verification from "./pages/Varification";
-import SmoothScroll from "./components/SmoothScoll";
 import NewPassword from "./pages/NewPassword";
+import AdminRoutes from "./pages/fairGameAdmin";
 import PageNotFound from "./components/PageNotFound";
-import NavigateLoader from "./components/NavigateLoader";
-
-const UserRoutes = React.lazy(() => import("./pages/matches/UserRoutes"));
-const MasterRoutes = React.lazy(() => import("./pages/master"));
-const ExpertRoutes = React.lazy(() => import("./pages/expert"));
-const AdminRoutes = React.lazy(() => import("./pages/fairGameAdmin"));
+import USerRoutes from "./pages/matches/UserRoutes";
+import SmoothScroll from "./components/SmoothScoll";
 
 const Main = () => {
   return (
     <AuthProvider>
-      <Suspense fallback={<NavigateLoader />}>
-        <SmoothScroll />
+      <SmoothScroll />
+      <Routes>
+        {/* User Routes */}
+        {<Route exact path="/*" element={<USerRoutes />} />}
+        {/* Master Routes */}
+        <Route exact path="/admin/*" element={<MasterRoutes />} />
 
-        <Routes>
-          {/* User Routes */}
-          {<Route exact path="/*" element={<UserRoutes />} />}
-          {/* Master Routes */}
-          <Route exact path="/admin/*" element={<MasterRoutes />} />
+        {/* Expert Routes */}
+        <Route exact path="/expert/*" element={<ExpertRoutes />} />
 
-          {/* Expert Routes */}
-          <Route exact path="/expert/*" element={<ExpertRoutes />} />
+        {/* admin Routes */}
+        <Route exact path="/wallet/*" element={<AdminRoutes />} />
 
-          {/* admin Routes */}
-          <Route exact path="/wallet/*" element={<AdminRoutes />} />
-
-          {/* <Route exact path="/fairgame_wallet/*" element={<FairGameWalletRoutes />} /> */}
-          {/* <Route path="/master/*" element={<MasterRoutes />} /> */}
-          {/* <Route path="/super_master/*" element={<SuperMasterRoutes />} /> */}
-          {/* <Route path="/super_admin/*" element={<SuperAdminRoutes />} /> */}
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Suspense>
+        {/* <Route exact path="/fairgame_wallet/*" element={<FairGameWalletRoutes />} /> */}
+        {/* <Route path="/master/*" element={<MasterRoutes />} /> */}
+        {/* <Route path="/super_master/*" element={<SuperMasterRoutes />} /> */}
+        {/* <Route path="/super_admin/*" element={<SuperAdminRoutes />} /> */}
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </AuthProvider>
   );
 };
