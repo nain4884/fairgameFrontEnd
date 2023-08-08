a809b3065ad36ea16c09e744e85bd526802777e6
ajay <101165102+ajay-duple@users.noreply.github.com>
2023-08-08 12:12:19 +0530

    #115

--- src/components/helper/PassKeyCheck.js
+++ src/components/helper/PassKeyCheck.js
@@ -1,83 +1,69 @@
 export const onChangeKeyCheck = (textValue) => {
-    let errorText = "Password must contain atleast "
-    let errorPush = []
+  let errorText = "Password must contain atleast ";
+  let errorPush = [];
 
-    var lowerCaseLetters = /[a-z]/g;
+  var lowerCaseLetters = /[a-z]/g;
 
-    if (textValue.match(lowerCaseLetters) === null) {
-        errorPush.push("one lowercase letter ")
-    }
+  if (textValue.match(lowerCaseLetters) === null) {
+    errorPush.push("one lowercase letter ");
+  }
 
-    var upperCaseLetters = /[A-Z]/g;
-    if (textValue.match(upperCaseLetters) === null) {
-        errorPush.push("one uppercase letter ")
-    }
+  var upperCaseLetters = /[A-Z]/g;
+  if (textValue.match(upperCaseLetters) === null) {
+    errorPush.push("one uppercase letter ");
+  }
 
+  var specialLetters = /[!@#$%^&*]/g;
+  if (textValue.match(specialLetters) === null) {
+    errorPush.push("one special letter ");
+  }
 
-    var specialLetters = /[!@#$%^&*]/g;
-    if (textValue.match(specialLetters) === null) {
-        errorPush.push("one special letter ")
-    }
+  var numbers = /[0-9]/g;
+  if (textValue.match(numbers) === null) {
+    errorPush.push("one numeric value ");
+  }
 
-    var numbers = /[0-9]/g;
-    if (textValue.match(numbers) === null) {
-        errorPush.push("one numeric value ")
-    }
+  if (textValue.length < 6) {
+    errorPush.push("six characters ");
+  }
 
-    if (textValue.length < 6) {
-        errorPush.push("six characters ")
-    }
-
-    if (errorPush.length === 1) {
-        errorText += errorPush[0]
+  if (errorPush.length === 1) {
+    errorText += errorPush[0];
+  } else {
+    if (errorPush.length === 2) {
+      errorText += errorPush[0] + "& " + errorPush[1];
     } else {
-        if (errorPush.length === 2) {
-            errorText += errorPush[0] + "& " + errorPush[1]
+      errorPush.forEach((err, i) => {
+        if (i + 2 === errorPush.length) {
+          errorText += err + "& ";
+        } else if (i + 1 === errorPush.length) {
+          errorText += err;
         } else {
-            errorPush.forEach((err, i) => {
-                if (i + 2 === errorPush.length) {
-                    errorText += err + "& "
-                } else if (i + 1 === errorPush.length) {
-                    errorText += err
-                } else {
-                    errorText += err + ", "
-                }
-            });
+          errorText += err + ", ";
         }
+      });
     }
+  }
 
-    if (errorText.length < 31) {
-        return false
-    } else {
-        return errorText
-    }
-
-}
+  if (errorText.length < 31) {
+    return false;
+  } else {
+    return errorText;
+  }
+};
 export const onChangeKeyCheckNumber = (textValue) => {
-    let errorText = "Password must contain at least ";
-    let errorPush = [];
+  let errorText = "Transaction password only 5 numeric digit";
 
-    var numbers = /^\d+$/;
-    if (!textValue.match(numbers)) {
-        errorPush.push("only numeric digits");
-    }
+  var numbers = "^[0-9]+$";
+  if (!textValue.match(numbers) && textValue.length <=5) {
+    return errorText;
+  }
+  if ( textValue.length <= 4) {
+    return errorText;
+  }
+  if ( textValue.length > 5) {
+    return errorText;
+  }
 
-    if (textValue.length < 4) {
-        errorPush.push("minimum 5 digits");
-    }
-
-    if (errorPush.length === 1) {
-        errorText += errorPush[0];
-    } else if (errorPush.length === 2) {
-        errorText += errorPush.join(" & ");
-    } else if (errorPush.length > 2) {
-        errorText +=
-            errorPush.slice(0, -1).join(", ") + " & " + errorPush.slice(-1);
-    }
-
-    if (errorPush.length === 0) {
-        return null; // Return null when there are no errors
-    } else {
-        return errorText;
-    }
+  return "";
 };
