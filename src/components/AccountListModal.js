1bbaf3005332d7b5f5e0ea75641c13cecccb5e1b
ajay <101165102+ajay-duple@users.noreply.github.com>
2023-08-17 12:36:58 +0530

    Update AccountListModal.js

--- src/components/AccountListModal.js
+++ src/components/AccountListModal.js
@@ -116,7 +116,7 @@ const AccountListModal = ({ id, show, setShow, title, handleExport }) => {
         sx={[
           {
             marginX: "0.5%",
-            width: { mobile: "96%", laptop: "85%", tablet: "96%" },
+            width: { mobile: "96%", laptop: "90%", tablet: "96%" },
             minHeight: loader && loader1 ? "20%" : "200px",
             borderRadius: "10px",
             borderBottomRightRadius: "0px",
